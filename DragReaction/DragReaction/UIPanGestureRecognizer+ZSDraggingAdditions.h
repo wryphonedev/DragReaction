5a0761ec48f2c0822a3d20539573e011990b9c61
Isaac Schmidt <isaac@getzuse.com>
2013-01-29 11:25:01 -0500

    Updated headers.

--- DragReaction/DragReaction/UIPanGestureRecognizer+ZSDraggingAdditions.h
+++ DragReaction/DragReaction/UIPanGestureRecognizer+ZSDraggingAdditions.h
@@ -17,15 +17,8 @@
 
 // Drag the recognizer's view within passed view. Execute overlap block when overlapping with subview in passed views array.
 - (void)dragWithinView:(UIView *)view evaluateOverlappingViews:(NSArray *)views overlapsBlock:(void (^)(UIView *overlapView))overlapsBlock completion:(void (^)(UIView *overlapView))completionBlock;
+
 // Drag the recognizer's view within passed view. Execute overlap block when overlapping with index in the passed rects array values.
 - (void)dragWithinView:(UIView *)view evaluateOverlappingRects:(NSArray *)rects overlapsBlock:(void (^)(NSUInteger overlapIndex))overlapsBlock completion:(void (^)(NSUInteger overlapIndex))completionBlock;
-// Given an array of CGRect values, returns the index of first rect overlapping with given point.
-- (NSUInteger)indexOfRectContainingPoint:(CGPoint)point evaluateRects:(NSArray *)evaluate;
-// Returns a view containing passed point.
-- (UIView *)viewContainingPoint:(CGPoint)point evaluateViews:(NSArray *)views;
-// Returns a view intersecting a passed rect.
-- (UIView *)viewIntersectingRect:(CGRect)rect evaluateViews:(NSArray *)views;
-// Returns a view containing a passed point.
-- (UIView *)viewContainingRect:(CGRect)rect evaluateViews:(NSArray *)views;
 
 @end
