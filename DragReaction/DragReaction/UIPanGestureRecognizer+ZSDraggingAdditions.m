3766519d91669f068cdaa6864c6112a643a473f4
Isaac Schmidt <isaac@getzuse.com>
2013-01-29 11:23:43 -0500

    Updated to use code to use new category on UIView.

--- DragReaction/DragReaction/UIPanGestureRecognizer+ZSDraggingAdditions.m
+++ DragReaction/DragReaction/UIPanGestureRecognizer+ZSDraggingAdditions.m
@@ -146,4 +146,78 @@ static char const * const StartPointKey = "StartPoint";
     objc_setAssociatedObject(self, StartPointKey, pointValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
+- (NSUInteger)indexOfRectContainingPoint:(CGPoint)point evaluateRects:(NSArray *)evaluate
+{
+    return [evaluate indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        
+        NSValue *rectValue = (NSValue *)obj;
+        CGRect rect = [rectValue CGRectValue];
+        
+        NSLog(@"evaluated rect:%@ and point:%@", NSStringFromCGRect(rect), NSStringFromCGPoint(point));
+        
+        return CGRectContainsPoint(rect, point);
+        
+    }];
+    
+}
+
+- (UIView *)viewContainingPoint:(CGPoint)point evaluateViews:(NSArray *)views
+{
+    // View containing a point
+    UIView *matchingView = nil;
+    NSInteger matchingIndex = [views indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        
+        UIView *view = (UIView *)obj;
+        CGRect frame = view.frame;
+        
+        return CGRectContainsPoint(frame, point);
+    }];
+    
+    if (matchingIndex != NSNotFound)
+    {
+        matchingView = [views objectAtIndex:matchingIndex];
+    }
+    
+    return matchingView;
+    
+}
+
+- (UIView *)viewIntersectingRect:(CGRect)rect evaluateViews:(NSArray *)views
+{
+    UIView *matchingView = nil;
+    NSInteger matchingIndex = [views indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        
+        UIView *view = (UIView *)obj;
+        CGRect frame = view.frame;
+        
+        return CGRectIntersectsRect(rect, frame);
+    }];
+    
+    if (matchingIndex != NSNotFound)
+    {
+        matchingView = [views objectAtIndex:matchingIndex];
+    }
+    
+    return matchingView;
+}
+
+- (UIView *)viewContainingRect:(CGRect)rect evaluateViews:(NSArray *)views
+{
+    UIView *matchingView = nil;
+    NSInteger matchingIndex = [views indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop)
+                               {
+                                   UIView *view = (UIView *)obj;
+                                   CGRect frame = view.frame;
+                                   
+                                   return CGRectContainsRect(frame, rect);
+                               }];
+    
+    if (matchingIndex != NSNotFound)
+    {
+        matchingView = [views objectAtIndex:matchingIndex];
+    }
+    
+    return matchingView;
+}
+
 @end
